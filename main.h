5c5854651f7ae8471708c264432a728cfb57a976
omama44 <emy4495.m@gmail.com>
2023-03-27 12:31:45 -0700

    update

--- main.h
+++ main.h
@@ -1,4 +1,27 @@
-VimCrypt~03!Wñ±Z!h@Š<y0P|\S÷ë¯fõğ|:èÿ–Õí8EMpœmæ™¸WîqÒ)ôs8q)uÖ“WPsQUF÷©%©•4fƒ İÍò£îOÿğL“R7Æ¡g°å(5ÏœË@iÂi±ñmÇI_(e•~R'ñÖE$ÉùfDîf£›¾b÷¶–äïI`´ûâÔpw'©#Rl7W;'y«µ¢îå·ÚÈ,J&Åá1±ö”\‘êá`‘¿3¤5]Zà¸›$ÌìÎÅ)İãŸå|ö	+Õî	ÎŸÕô> i>KSŠ@û§ƒ½àò´
-äÛMë†Ø<Yñ“¿#2Ş°şñ\å"Ú¦›f+Âú©_hÄ ao@æ(±À"~ıâÖKÕ&Jjìšf|dqùœ*™Vo.†’Ì‡Ã/ôÏÕtŒÑh±Õâˆ¢¹˜ˆš&øu*n£éæ5`ŸÑâãEuÁŒÙšã›«! f
-ä…hxÍŠ×w™^_¢;¯5ëL9à÷éé‹!=µµå¸diÔÒa!Î—'Gğj˜	1¤cEõ«òä“—GQ¼Š‡wË£A	¡ÅåuÈ<i—T$_P¬[¸fÏäõVˆ•2–†•OŞ]öo©°6g
-",ÿ?sşô>ÀQš‚w³ÄKt£ö?‘H(¤°Î/]v›_>¶lnŸÊãÙGXÈÏg6 ë¢g~"¢åÄ0´‚†ù¸·jRü¤°.Ü-¶ßŠÆâ»Ûx	¬íc¸nç¤]ƒøÂ1vƒæqYÆbäÂĞ"‚IUM¤¿²´¢{°x$ğ•.ÑG>|FI•8C)©šeÉÖ¥›8È2úí.øiÜëì¶©KÜM¾‡,¼cx“xıèÀv5/vÙÂ5¦}óº6å
+#include <stdarg.h>
+
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
+
+int _write(char c);
+int _printf(const char *format, ...);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
+
+#endif
